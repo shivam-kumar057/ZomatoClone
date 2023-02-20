de66ca9e08ca31bb8aa720f4e58baacc86180d63
shivam <shivamkmr057@gmail.com>
2023-02-20 22:12:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet,View,Text, SafeAreaView } from "react-native";
+
+const App = () =>{
+    return (
+        <SafeAreaView style={{flex:1,backgroundColor:'red'}}>
+
+        </SafeAreaView>
+    )
+}
+
+export default App
